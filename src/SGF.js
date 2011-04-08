85b2112094ea08847879391eecefc23b5c5c85f2
Nathan Rajlich <nathan@tootallnate.net>
2011-04-07 19:21:33 -0700

    Add the 'SGF.js' file. Empty currently.

---
+++ src/SGF.js
@@ -0,0 +1,14 @@
+// This is the SGF bootstrap file. It defines the webpage-visible
+// 'SGF' object.
+// TODO: Ensure's that ModuleJS is loaded.
+
+(function(window, undefined) {
+
+  // The 'SGF' namespace is really just 
+  var SGF = window['SGF'] = {};
+
+  function createGame(path, container, callback) {
+
+  }
+  SGF['createGame'] = createGame;
+})(this);
